4c15d9cc32c833aea0c82ecd7c325b33476ce2e7
e0641591 <e0641591@u.nus.edu>
2020-09-29 17:56:23 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SA51_CA_Project_Team10.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
