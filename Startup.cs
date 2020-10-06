ac29736bc81e925865fc5b8e933dac6889e8e7d1
e0641591 <e0641591@u.nus.edu>
2020-10-06 19:37:41 +0800

    change default layout

--- Startup.cs
+++ Startup.cs
@@ -59,7 +59,7 @@ namespace SA51_CA_Project_Team10
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
 
-            db.Database.EnsureDeleted(); // Comment this line away if you don't want to restart the database every time
+            //db.Database.EnsureDeleted(); // Comment this line away if you don't want to restart the database every time
             db.Database.EnsureCreated();
 
             new DbSeeder(db).Seed();
