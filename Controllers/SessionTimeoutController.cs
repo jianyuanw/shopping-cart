94d92326db955450730d5501d026092601d8a61c
Angelia Lestari <e0641592@u.nus.edu>
2020-10-10 17:04:07 +0800

    Add session keeper middle ware

---
+++ Controllers/SessionTimeoutController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SA51_CA_Project_Team10.DBs;
+using SA51_CA_Project_Team10.Models;
+
+namespace SA51_CA_Project_Team10.Controllers
+{
+    public class SessionTimeoutController : Controller
+    {
+        private readonly DbT10Software _db;
+
+        public SessionTimeoutController(DbT10Software db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            string sessionId = HttpContext.Request.Cookies["sessionId"];
+
+            _db.Sessions.Remove(new Session()
+            {
+                Id = sessionId
+            });
+            _db.SaveChanges();
+
+            HttpContext.Response.Cookies.Delete("sessionId");
+
+            TempData["Alert"] = "primary|Your session has timed-out!";
+            return Redirect("/Gallery/Index");
+        }
+    }
+}
