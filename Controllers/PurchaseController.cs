ee5bf9af5e75a83bc31960475f16c9270853bc73
jianyuanw <69199572+jianyuanw@users.noreply.github.com>
2020-10-10 23:22:57 +0800

    Added a check for customers with no past purchases. Display a message for them.

--- Controllers/PurchaseController.cs
+++ Controllers/PurchaseController.cs
@@ -22,25 +22,36 @@ namespace SA51_CA_Project_Team10.Controllers
             // Retrieve session
             Session session = _db.Sessions.FirstOrDefault(x => x.Id == Request.Cookies["sessionId"]);
 
-            // If session exists, means user is logged in.
-            if (session != null)
+            if (session != null) // If session exists, means user is logged in.
             {
                 // Logout button
                 ViewData["Logged"] = true;
+
+                // Bold menu item
+                ViewData["Is_Purchase"] = "font-weight: bold";
 
                 User user = _db.Users.FirstOrDefault(x => x.Id == session.UserId);
 
                 // Display username at top left
                 ViewData["Username"] = user.Username;
 
-                // Retrieve OrderDetails, Orders, Products from DB
+                // Retrieve OrderDetails from DB
                 List<OrderDetail> orderDetails = _db.OrderDetails.ToList();
 
-                // Join lists. Filter based on UserId. Order by date. Select relevant columns.
+                // Filter based on UserId. Order by date. Select and group by relevant columns.
                 var purchases = orderDetails.OrderByDescending(od => od.Order.DateTime)
                                             .Where(od => od.Order.UserId == user.Id)
                                             .Select(od => new { od.Product.ImageLink, od.Product.Name, od.Product.Description, od.Order.DateTime, od.Id })
                                             .GroupBy(d => new { d.ImageLink, d.Name, d.Description, d.DateTime }).ToList();
+
+                if (purchases.Count == 0) // If no purchases, send info to View to display "no past purchases"
+                {
+                    ViewData["havePastOrders"] = false;
+
+                    return View();
+                }
+
+                ViewData["havePastOrders"] = true;
 
                 var model = new PurchasesViewModel();
 
@@ -66,9 +77,6 @@ namespace SA51_CA_Project_Team10.Controllers
                     COD.Ids = list;
                     model._products.Add(COD);
                 }
-
-                // Bold menu item
-                ViewData["Is_Purchase"] = "font-weight: bold";
 
                 return View(model);
             }
