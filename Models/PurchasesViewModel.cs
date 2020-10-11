f9ea5f05042d1f35af18fde4381468f54c05edcf
MokZF404 <54626382+MokZF404@users.noreply.github.com>
2020-10-11 11:28:45 +0800

    Create PurchasesViewModel.cs

---
+++ Models/PurchasesViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SA51_CA_Project_Team10.Models
+{
+    public class PurchasesViewModel
+    {
+        public List<ConciseOrderDetail> _products;
+
+        public PurchasesViewModel()
+        {
+            _products = new List<ConciseOrderDetail>();
+        }
+
+    }
+
+    public class ConciseOrderDetail
+    {
+        public string ImageLink { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int Quantity { get; set; }
+        public List<string> Ids;
+    }
+}
